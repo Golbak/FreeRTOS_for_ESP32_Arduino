15f1e3de1c26ea2b7170b6ce983fd7d2746a83c1
Golbak <r.zordek@hotmail.com>
2021-10-02 12:57:53 +0200

    Add Yielding Exercise
    Added exercise to test the effect of Yielding in the tasks on FreeRTOS

---
+++ 24_Yielding/24_Yielding.ino
@@ -0,0 +1,50 @@
+#include <FreeRTOS.h>
+
+#define GPIO  12
+
+static void gpio_high(void *argp) {
+  for(;;){
+    for(short x=0; x<1000; ++x)
+      digitalWrite(GPIO, HIGH);
+    taskYIELD();
+  }
+}
+
+static void gpio_low(void *argp) {
+  for(;;){
+    digitalWrite(GPIO, LOW);
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  int iApp_cpu = xPortGetCoreID();
+
+  pinMode(GPIO, OUTPUT);
+  delay(1000);
+  printf("Setup started..\n");
+  
+  xTaskCreatePinnedToCore(
+    gpio_high,
+    "gpio_high",
+    2048,
+    nullptr,
+    1,
+    nullptr,
+    iApp_cpu
+  );
+
+  xTaskCreatePinnedToCore(
+    gpio_low,
+    "gpio_low",
+    2048,
+    nullptr,
+    1,
+    nullptr,
+    iApp_cpu
+  );
+}
+
+void loop() {
+  vTaskDelete(xTaskGetCurrentTaskHandle());
+}
